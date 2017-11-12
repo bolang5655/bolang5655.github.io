2caa5fe201bc2e8c0e785d2a7673461b05e354d3
bolang5655.github.io <torisjhon@gmail.com>
2017-11-12 08:44:38 +0700

    update1

---
+++ index.js
@@ -0,0 +1,22 @@
+
+
+// string 
+const apos_string ='hello':
+const quot_string ="hello"
+const tick_string = 'hello'
+
+
+// boolean
+
+const b =true
+
+x = boolean(-1)
+
+// array
+
+r = [n,n,n,n,n,];
+
+rs = Array.from (apos_string)
+
+Console.log(appos_string.length)
+ 
